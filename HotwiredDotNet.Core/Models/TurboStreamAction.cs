7d9a361dc12bb1ad873195c0535aee543f6f512c
Tobias Mønster <tobias.monster@akqa.com>
2023-03-29 12:40:15 +0200

    Adds XML doc to Turbo Stream Actions

--- HotwiredDotNet.Core/Models/TurboStreamAction.cs
+++ HotwiredDotNet.Core/Models/TurboStreamAction.cs
@@ -1,15 +1,43 @@
 ﻿namespace HotwiredDotNet.Core.Models
 {
+    /// <summary>
+    /// Represents the action to perform on a <see href="https://turbo.hotwired.dev/reference/streams#the-seven-actions">turbo-stream target</see>. 
+    /// </summary>
     public enum TurboStreamAction
     {
-#pragma warning disable SA1602 // Enumeration items should be documented
+        /// <summary>
+        /// <see href="https://turbo.hotwired.dev/reference/streams#append">Append</see> Append the template to the target.
+        /// </summary>
         Append,
+
+        /// <summary>
+        /// <see href="https://turbo.hotwired.dev/reference/streams#prepend">Prepend</see> the template to the target.
+        /// </summary>
         Prepend,
+
+        /// <summary>
+        /// <see href="https://turbo.hotwired.dev/reference/streams#replace">Replace</see> the target with the template.
+        /// </summary>
         Replace,
+
+        /// <summary>
+        /// <see href="https://turbo.hotwired.dev/reference/streams#update">Update</see> the target with the template.
+        /// </summary>
         Update,
+
+        /// <summary>
+        /// <see href="https://turbo.hotwired.dev/reference/streams#remove">Remove</see> the target.
+        /// </summary>
         Remove,
+
+        /// <summary>
+        /// Insert the template  <see href="https://turbo.hotwired.dev/reference/streams#before">before</see> the target.
+        /// </summary>
         Before,
-        After,
-#pragma warning restore SA1602 // Enumeration items should be documented
+
+        /// <summary>
+        /// Insert the template <see href="https://turbo.hotwired.dev/reference/streams#after">after</see> the target.
+        /// </summary>
+        After
     }
 }
